3777b7b64a79086cbd9277d39abe44d85ad28ce8
kplnhsyn <kplnhsyn.49@gmail.com>
2024-03-04 02:08:50 +0100

    feat:App.jsx wrapped with provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import AppRouter from "./router/AppRouter"
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AppRouter from "./router/AppRouter";
+import { grey, blueGrey } from "@mui/material/colors";
 
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import store from "./app/store";
 
 function App() {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: grey["900"],
+            },
+            secondary: {
+                main: blueGrey["900"],
+            },
+        },
+    });
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <Provider store={store}>
+                    <AppRouter />
 
-  return (
-    <>
-     <AppRouter/>
-    </>
-  )
+                    <ToastContainer />
+                </Provider>
+            </ThemeProvider>
+        </>
+    );
 }
 
-export default App
+export default App;
